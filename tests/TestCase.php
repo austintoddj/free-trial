0b6b50c1cbeb05c0e041824fd68040f83c08b4bb
Todd Austin <austin.todd.j@gmail.com>
2019-05-04 05:55:42 -0500

    added a unit test for the email validation method

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,31 @@
 
 namespace Tests;
 
+use ReflectionClass;
+use ReflectionException;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Call the protected or private methods of a class.
+     *
+     * @param $object
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     * @throws ReflectionException
+     */
+    protected function invokeMethod(&$object, string $method, array $parameters = [])
+    {
+        $reflection = new ReflectionClass(get_class($object));
+
+        $method = $reflection->getMethod($method);
+
+        $method->setAccessible(true);
+
+        return $method->invokeArgs($object, $parameters);
+    }
 }
---
+++ tests/Unit/Http/Controllers/VerificationTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers;
+
+use Tests\TestCase;
+use App\Http\Controllers\VerificationController;
+
+class VerificationTest extends TestCase
+{
+    /**
+     * The controller instance.
+     *
+     * @var VerificationController
+     */
+    protected $instance;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->instance = new VerificationController();
+    }
+
+    /** @test */
+    public function filter_invalid_emails()
+    {
+        $valid_email = $this->invokeMethod($this->instance, 'isValid', ['example@example.com']);
+
+        $this->assertTrue($valid_email);
+
+        $invalid_email = $this->invokeMethod($this->instance, 'isValid', ['example']);
+
+        $this->assertFalse($invalid_email);
+    }
+}
